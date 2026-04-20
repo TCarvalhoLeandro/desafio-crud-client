4617f007922b41d2befba3ca75256282c2c2af39
Leandro <trairicarvalho@gmail.com>
2026-04-20 17:13:34 -0300

    feat: seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,9 @@ INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vera Gr
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('John Grey', '56987452365', 7500.0, '1984-12-23', 1);
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Suzy Purple', '45871256321', 8500.0, '1974-05-08', 3);
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bob Brown', '89654123654', 9500.0, '1964-03-14', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jhony Bravo', '85478569321', 7500.0, '1954-04-24', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Smith Black', '36521452365', 3500.0, '2000-03-05', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Joseph Yelow','78547856985', 8500.0, '2006-09-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Robert Blue', '36523654785', 7500.0, '1974-12-11', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roger Sink', '96587412563', 5500.0, '1945-03-05', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Steven Well', '98563214563', 9500.0, '1987-08-24', 2);
